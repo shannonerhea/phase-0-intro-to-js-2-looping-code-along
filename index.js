3828ef8adb9b395e4da3776796d3ee82488df550
shannone <shannone426@gmail.com>
2022-05-26 12:36:54 -0400

    3passing

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     //debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+function writeCards(arr, event) {
+let newwArr = [];
+for (let i = 0; i < arr.length; i++) {
+    let msg = (`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+newwArr.push(msg);
+};
+return newwArr;
+}
+
+function countDown(int) {
+    let i = 11;
+    while (i > 0) {
+        i--
+        console.log(i);
+    };
+    return int;
+};
